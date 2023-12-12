d0744b03c3125aaed89a7024a15660708b277f33
Eliel <125eramirez@frhsd.com>
2023-12-12 08:31:21 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+let myButton = document.getElemtById('theBtn');
+myButton.addEventzlister('click', addH1);
+
+function addH1()
+{
+    let newH1 = innerHtml = "New H1 Here" ;
+    newH1.innerHtml = "New H1 Here"
+    document.body.appendChild(NewH1)
+}
